d1c8372e6adf511490958c08133bb70294527791
Enzo <enzogallardosimon@gmail.com>
2026-09-23 01:41:40 +0200

    test: add database connection test

--- src/test/java/com/agenda/common/persistence/DatabaseConnectionTest.java
+++ src/test/java/com/agenda/common/persistence/DatabaseConnectionTest.java
@@ -1,4 +1,22 @@
+package com.agenda.common.persistence;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DatabaseConnectionTest {
-  
+
+    @Test
+    void shouldConnectToDatabase() throws SQLException {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+
+            assertNotNull(connection);
+            assertFalse(connection.isClosed());
+            assertEquals("agenda-app-database", connection.getCatalog());
+        }
+    }
+
 }
